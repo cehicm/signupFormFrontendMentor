07181a33b36ec11c756238af33cd896ad9af09c7
Marija Cehic <cehicmarija@gmail.com>
2021-01-27 02:13:44 +0100

    finished css, need to figure out js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // add a function for input field:
-//check for blanks, 
+//check for blanks,
 // create an onblur and onfocus function
+
+function submitForm() {
+  let passwordInput = document.getElementById("passwordField").value;
+  let inputErrorMessage = document.getElementById("inputErrorMessage");
+  let fieldInput = document.getElementsByClassName("");
+
+  if (fieldInput.length < 5) {
+  }
+}
