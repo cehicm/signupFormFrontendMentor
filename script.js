6db62fb31ece3d7704165260b397463ce21d6629
Marija Cehic <cehicmarija@gmail.com>
2021-01-30 01:02:19 +0100

    started pass checking functionality;

--- script.js
+++ script.js
@@ -4,9 +4,33 @@
 
 function submitForm() {
   let passwordInput = document.getElementById("passwordField").value;
-  let inputErrorMessage = document.getElementById("inputErrorMessage");
-  let fieldInput = document.getElementsByClassName("");
+  let inputErrorMessage = document.getElementById("passErrorMessage");
 
-  if (fieldInput.length < 5) {
+  let fieldInput = document.querySelectorAll("singleInputField").value;
+
+  if (passwordInput.length < 5) {
+    inputErrorMessage.innerHTML += "Cant Be Empty" + "<br>";
+  }
+
+  let checkForNumbers = false;
+  let checkForCapitalLetters = false;
+
+  for (let i = 0; i < passwordInput.length; i++) {
+    let singleCharacter = passwordInput[i];
+
+    if (!isNaN(singleCharacter)) {
+      checkForNumbers = true;
+    } else if (singleCharacter === singleCharacter.toUpperCase()) {
+      checkForCapitalLetters == true;
+    }
+
+    if (checkForNumbers === false) {
+      inputErrorMessage.innerHTML =
+        "The passwords is missing a number" + "<br>";
+    }
+
+    if (checkForCapitalLetters === false) {
+      inputErrorMessage += " The password is missing a capital letter" + "<br>";
+    }
   }
 }
